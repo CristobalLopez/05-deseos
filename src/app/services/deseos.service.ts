7f7c857f19a2aa4685d45c16cc5c02cb97885177
CristobalLopez <clopezosses@gmail.com>
2020-02-06 18:03:02 -0300

    commit hasta video 123

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -12,12 +12,43 @@ export class DeseosService {
   
   constructor() {
 
-    const LISTA1= new Lista("Recolectar piedras del Infinito");
-    const LISTA2= new Lista("Heroes a desaparecer");
-    this.listas.push(LISTA1, LISTA2);
-
-    console.log(this.listas);
+    this.cargarStorage();
+    
    }
+
+   crearLista(titulo: string ){
+
+    const nuevaLista = new Lista(titulo);
+    this.listas.push(nuevaLista);
+    this.guardarStorage();
+    return nuevaLista.id;
+   }
+
+   obtenerLista( id: string | number){
+     id = Number(id);
+
+     return this.listas.find(listaData => listaData.id === id );
+
+   }
+
+ guardarStorage(){
+
+  localStorage.setItem('data', JSON.stringify(this.listas));
+
+ }
+
+ cargarStorage(){
+
+  if (localStorage.getItem('data')) {
+
+    this.listas = JSON.parse(localStorage.getItem('data'));
+    
+  } else {
+    this.listas= [];
+  }
+   
+
+ }
 
 
 }
